d2dd5facfeb6c73d68120cedc07f2fed79fc2c95
Akars <williamli973@gmail.com>
2020-11-02 17:09:51 +0100

    solution de secours

--- main.py
+++ main.py
@@ -1,38 +1,23 @@
 class Graph:
     nb_vertix = 0
     nb_edge = 0
-    edges = [[]]
+    adjMatrix = [[]]
     
-# #Set Graph from a file
-# def setGraph(index):
-#     f = open(index + ".txt", "r")
-#     graph = Graph()
-#     graph.nb_vertix = int(f.readline())
-#     graph.nb_edge = int(f.readline())
-#     newTable = dict() {"key": "valeur", "key": "valeur"}
-#     for i in range(graph.nb_edge):
-#         temp = int(f.readline(1))
-#         tempArray = [int(f.readline(3)), int(f.readline(5))]
-#         if graph.edges.get(temp) != None:
-#             graph.edges.get(temp).append(tempArray)
-#         else:
-#             graph.edges.update({temp: [tempArray]})
-#     return graph
-
 def setGraph(index):
     f = open("Graph/" + index + ".txt", "r")
     graph = Graph()
     graph.nb_vertix = int(f.readline())
     graph.nb_edge = int(f.readline())
-    graph.edges = [[float('inf') for i in range(graph.nb_vertix)] for j in range(graph.nb_vertix)]
+    graph.adjMatrix = [[float('inf') for i in range(graph.nb_vertix)] for j in range(graph.nb_vertix)]
     for x in range(graph.nb_edge):
         i = int(f.readline(1))
         j = int(f.readline(3))
-        graph.edges[i][j] = int(f.readline(5))
+        graph.adjMatrix[i][j] = int(f.readline(5))
     for i in range(graph.nb_vertix):
         for j in range(graph.nb_vertix):
             if(i == j):
-                graph.edges[i][j] = 0
+                graph.adjMatrix[i][j] = 0
+    f.close()
     return graph
 
 def printAdjacencyMatrix(graph):
@@ -43,32 +28,32 @@ def printAdjacencyMatrix(graph):
     for i in range(graph.nb_vertix):
         print(i, end = '\t')
         for j in range(graph.nb_vertix):
-            print(graph.edges[i][j], end = '\t')
+            print(graph.adjMatrix[i][j], end = '\t')
         print('')
     
-def floydWarshall(adjMatrix, N):
+def floydWarshall(graph):
 
 	# cost and parent matrix stores shortest-path
 	# (shortest-cost/shortest route) information
 
 	# initially cost would be same as weight of the edge
-	cost = adjMatrix.copy()
-	path = [[None for x in range(N)] for y in range(N)]
+	cost = graph.adjMatrix.copy()
+	path = [[None for x in range(graph.nb_vertix)] for y in range(graph.nb_vertix)]
 
 	# initialize cost and parent
-	for v in range(N):
-		for u in range(N):
-			if v == u:
-				path[v][u] = 0
-			elif cost[v][u] != float('inf'):
-				path[v][u] = v
+	for i in range(graph.nb_vertix):
+		for j in range(graph.nb_vertix):
+			if i == j:
+				path[i][j] = 0
+			elif cost[i][j] != float('inf'):
+				path[i][j] = i
 			else:
-				path[v][u] = -1
+				path[i][j] = -1
 
 	# run Floyd-Warshall
-	for k in range(N):
-		for v in range(N):
-			for u in range(N):
+	for k in range(graph.nb_vertix):
+		for v in range(graph.nb_vertix):
+			for u in range(graph.nb_vertix):
 				# If vertex k is on the shortest path from v to u,
 				# then update the value of cost[v][u], path[v][u]
 				if cost[v][k] != float('inf') and cost[k][u] != float('inf') \
@@ -83,30 +68,30 @@ def floydWarshall(adjMatrix, N):
 				return
 
 	# Print the shortest path between all pairs of vertices
-	printSolution(path, N)
+	printSolution(path, graph.nb_vertix)
 
-def printSolution(path, N):
+def printSolution(path, nb_vertex):
 
-	for v in range(N):
-		for u in range(N):
-			if u != v and path[v][u] != -1:
-				print(f"Shortest Path from {v} -> {u} is ({v}", end=' ')
-				printPath(path, v, u)
-				print(f"{u})")
+	for i in range(nb_vertex):
+		for j in range(nb_vertex):
+			if i != j and path[i][j] != -1:
+				print(f"Shortest Path from {i} -> {j} is ({i}", end=' ')
+				printPath(path, i, j)
+				print(f"{j})")
 
-def printPath(path, v, u):
+def printPath(path, i, j):
 
-	if path[v][u] == v:
+	if path[i][j] == i:
 		return
 
-	printPath(path, v, path[v][u])
-	print(path[v][u], end=' ')
+	printPath(path, i, path[i][j])
+	print(path[i][j], end=' ')
 
 def main():
     newGraph = setGraph("1")
-    print(newGraph.edges)
+    print(newGraph.adjMatrix)
     printAdjacencyMatrix(newGraph)
-    floydWarshall(newGraph.edges, newGraph.nb_vertix)
+    floydWarshall(newGraph)
 
 if __name__ == "__main__":
     main()
