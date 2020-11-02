bb85d3e014fba6fa5622bbbff2d7c24a9dc648df
Akars <williamli973@gmail.com>
2020-11-02 16:54:16 +0100

    solution de secours

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def setGraph(index):
     graph = Graph()
     graph.nb_vertix = int(f.readline())
     graph.nb_edge = int(f.readline())
-    graph.edges = [['∞' for i in range(graph.nb_vertix)] for j in range(graph.nb_vertix)]
+    graph.edges = [[float('inf') for i in range(graph.nb_vertix)] for j in range(graph.nb_vertix)]
     for x in range(graph.nb_edge):
         i = int(f.readline(1))
         j = int(f.readline(3))
@@ -46,10 +46,67 @@ def printAdjacencyMatrix(graph):
             print(graph.edges[i][j], end = '\t')
         print('')
     
+def floydWarshall(adjMatrix, N):
+
+	# cost and parent matrix stores shortest-path
+	# (shortest-cost/shortest route) information
+
+	# initially cost would be same as weight of the edge
+	cost = adjMatrix.copy()
+	path = [[None for x in range(N)] for y in range(N)]
+
+	# initialize cost and parent
+	for v in range(N):
+		for u in range(N):
+			if v == u:
+				path[v][u] = 0
+			elif cost[v][u] != float('inf'):
+				path[v][u] = v
+			else:
+				path[v][u] = -1
+
+	# run Floyd-Warshall
+	for k in range(N):
+		for v in range(N):
+			for u in range(N):
+				# If vertex k is on the shortest path from v to u,
+				# then update the value of cost[v][u], path[v][u]
+				if cost[v][k] != float('inf') and cost[k][u] != float('inf') \
+						and (cost[v][k] + cost[k][u] < cost[v][u]):
+					cost[v][u] = cost[v][k] + cost[k][u]
+					path[v][u] = path[k][u]
+
+			# if diagonal elements become negative, the
+			# graph contains a negative weight cycle
+			if cost[v][v] < 0:
+				print("Negative Weight Cycle Found")
+				return
+
+	# Print the shortest path between all pairs of vertices
+	printSolution(path, N)
+
+def printSolution(path, N):
+
+	for v in range(N):
+		for u in range(N):
+			if u != v and path[v][u] != -1:
+				print(f"Shortest Path from {v} -> {u} is ({v}", end=' ')
+				printPath(path, v, u)
+				print(f"{u})")
+
+def printPath(path, v, u):
+
+	if path[v][u] == v:
+		return
+
+	printPath(path, v, path[v][u])
+	print(path[v][u], end=' ')
+
 def main():
     newGraph = setGraph("1")
     print(newGraph.edges)
     printAdjacencyMatrix(newGraph)
+    floydWarshall(newGraph.edges, newGraph.nb_vertix)
 
 if __name__ == "__main__":
     main()
